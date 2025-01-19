1231e2ae3faa008d01aa3f5d1335312828f8e501
Dantesos33 <dantesos33@gmail.com>
2025-01-19 21:02:16 +0500

    added frontend and backend

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const cors = require('cors');
+const fs = require("fs");
+const users = require("./data.json");
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+
+//Http Methods
+app.get("/", (req, res) => {
+  return res.sendFile("index.html", { root: __dirname });
+});
+
+app.get("/users", (req, res) => {
+  return res.json(users);
+});
+
+app.route("/users/:id").get((req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+  if(!user) return res.status(404).json( {message: "user not found", id: id} );
+  else return res.send(user);
+}).post((req, res) => {
+  const id = Number(req.params.id);
+  if (!users.find((user) => user.id === id)) return res.status(404).json( {message: "user not found", id: id} )
+  const user = users.indexOf(id);
+  users.splice(user, 1);
+  fs.writeFile("./data.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "Success", id: id });
+  });
+})
+
+app.post("/users", (req, res) => {
+  const body = req.body;
+  if (!body || !body.name || !body.email || !body.gender)
+    return res.status(400).json({ message: "Fields are empty" });
+  users.push({ id: users.length + 1, ...body });
+  fs.writeFile("./data.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "Success", id: users.length });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Running on \u001b\\http://localhost:8000/\u001b\\`);
+});
